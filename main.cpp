efedc2c8031d58a2bcbb176505b4c878e4600c9b
Joshua Goring <qualitysoftwareguy@gmail.com>
2026-01-26 13:51:57 -0600

    Update basic information for the project

--- main.cpp
+++ main.cpp
@@ -4,8 +4,12 @@
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+    QApplication app(argc, argv);
+    // TODO: Use the values set in CMakeLists.txt (after making them useable) instead of hard-coding them here
+    QApplication::setApplicationName("Impulse PHM");
+    QApplication::setApplicationVersion("0.1.0");
+
+    MainWindow main_window;
+    main_window.show();
+    return app.exec();
 }
